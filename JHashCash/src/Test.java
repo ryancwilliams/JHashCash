6f146e1b97ce423de4a9159a8c92d4a49f627b63
Ryan Williams <rcw4rcw@gmail.com>
2012-12-20 22:29:17 -0500

    Created Test Program

--- JHashCash/src/Test.java
+++ JHashCash/src/Test.java
@@ -1,6 +1,7 @@
 
 import java.security.NoSuchAlgorithmException;
 import com.github.ryancwilliams.JHashCash.HashCash;
+import java.util.Scanner;
 
 /*
  * To change this template, choose Tools | Templates
@@ -17,12 +18,24 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the text you want to secure: ");
+        String data = scan.nextLine();
+        System.out.println("Enter the target securty level: ");
+        int targetLevel = scan.nextInt();
+        
+        long startTime = System.currentTimeMillis();
+        
         HashCash hc = new HashCash();
         
-        //System.out.println(hc.getSecurityLevel("test::AvTP4OWRLMXzhYFT"));
+        //System.out.println(hc.getSecurityLevel("test::5ffcgihoARk7xIqu"));
         
-        String hashCash = hc.GenerateHashCash("test", 20);
+        String hashCash = hc.GenerateHashCash(data, targetLevel);
         
-        System.out.println(hashCash + " Security Level : " + hc.getSecurityLevel(hashCash));
+        long stopTime = System.currentTimeMillis();
+        double timeSec = (double) (stopTime - startTime) / 1000;
+        System.out.println(hashCash + " Security Level : " + hc.getSecurityLevel(hashCash) + 
+                " in " + timeSec + " seconds");
+        
     }
 }
